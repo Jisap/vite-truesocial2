e053db82ca379d7a3b9faaff7b386d1c2538a115
Jorge Matos <jisapjisap@gmail.com>
2026-09-02 12:41:37 +0200

    feat: create About page with PageHeader, ScrollingTicker, and AboutUs components

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+
+import AboutUs from "../components/sections/shared/AboutUs";
+import ScrollingTicker from "../components/sections/shared/ScrollingTicker";
+import PageHeader from "../components/ui/PageHeader";
 
 const About = () => {
-    return (
-        <div>About</div>
-    )
+  return (
+    <>
+      <PageHeader
+        title="about"
+        accent="us"
+        current="about us"
+      />
+
+      <ScrollingTicker />
+
+      <AboutUs />
+    </>
+  )
 }
 
 export default About
