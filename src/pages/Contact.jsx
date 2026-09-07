d2c921c21ee75569bb122ef218ea0b2a60289b00
Jorge Matos <jisapjisap@gmail.com>
2026-09-07 12:41:29 +0200

    feat: implement contact page structure with form, map, and info components

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+
 import PageHeader from '../components/ui/PageHeader'
 import ScrollingTicker from '../components/sections/shared/ScrollingTicker'
+import ContactInfo from '../components/sections/contact/ContactInfo'
+import SectionTitle from '../components/ui/SectionTitle'
 
 const Contact = () => {
   return (
@@ -12,6 +14,25 @@ const Contact = () => {
       />
 
       <ScrollingTicker />
+      <ContactInfo />
+
+      <div className='py-20 lg:py-[100px]'>
+        <div className='container-custom'>
+          <div className='mb-12 grid gap-6 border-b border-primary/20 lg:grid-cols-12'>
+            <SectionTitle
+              eyebrown="Contact us"
+              title="Get in"
+              accent="touch whith us"
+              className='mb-0 lg:col-span-5'
+            />
+
+            <p className='mb-0 lg:col-span-5'>
+              Connect with our team for tailored social media marketing solutions that elevate your brand, engage your
+              audience, and drive result.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
