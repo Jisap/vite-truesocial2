023a8cd55b644dbfb916726c4a419cb4c43ee132
Jorge Matos <jisapjisap@gmail.com>
2026-09-07 12:29:20 +0200

    feat: add FaqSidebar component with category navigation and call-to-action section

--- src/components/sections/faqs/FaqSidebar.jsx
+++ src/components/sections/faqs/FaqSidebar.jsx
@@ -1,12 +1,30 @@
-import AnimatedText from "../../../components/ui/AnimatedText"
-import AccordionItem from "../../../components/ui/AccordionItem"
+import Reveal from "../../../components/ui/Reveal"
+import SidebarCta from "../../../components/ui/SidebarCta"
+import { faqCategories } from "@/data/faqCategories"
 
 
 
 const FaqSidebar = () => {
   return (
     <>
+      <aside className="flex flex-col gap-8 lg:sticky lg:top-32">
+        <Reveal className="rounded-[20px] border border-divider p-8">
+          <ul className="m-0 flex list-none flex-col gap-4 p-0">
+            {faqCategories.map((cat) => (
+              <li
+                key={cat.id}
+                className="border-b border-divider pb-4 capitalize last:border-0 last:pb-0"
+              >
+                <a href={`#${cat.id}`} className="text-body transition-colors duration-300 hover:text-accent">
+                  {cat.navLabel}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </Reveal>
 
+        <SidebarCta delay={0.25} />
+      </aside>
     </>
   )
 }
